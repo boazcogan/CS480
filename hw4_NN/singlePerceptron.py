0696bb197ae92c3ef23662ce4973264f2b0c1f61
boazcogan <boazcogan@yahoo.com>
2018-11-28 20:01:02 -0800

    pushing functional perceptron

--- hw4_NN/singlePerceptron.py
+++ hw4_NN/singlePerceptron.py
@@ -1,101 +1,101 @@
 import numpy as np
 import generate_labels
-import tqdm
+from random import shuffle
+
+def loadData():
+    print("Loading train and test data...")
+    # Load the data and generate labels for 7 and 9
+    # Since it is basic perceptron, The labels are binary, 1 or 0
+    # in this case, 7s are 1, 9s are 0
+    all_7_data = generate_labels.LoadAllLabelsOfNumber(7)
+    # all_7_labels = np.ones((len(all_7_data)))
+    all_9_data = generate_labels.LoadAllLabelsOfNumber(9)
+    # all_9_labels = np.zeros((len(all_9_data)))
+
+
+    train7 = all_7_data[:(4 * len(all_7_data)) // 5]
+    test7 = all_7_data[(4 * len(all_7_data)) // 5:]
+    labelsTrain7 = np.ones(len(train7))
+    labelsTest7 = np.ones(len(test7))
+
+
+
+    train9 = all_9_data[:(4 * len(all_9_data)) // 5]
+    test9 = all_9_data[(4 * len(all_9_data)) // 5:]
+    labelsTrain9 = np.zeros(len(train9))
+    labelsTest9 = np.zeros(len(test9))
+
+
+    TRAIN_DATA = np.array(train7 + train9)
+    TRAIN_LABELS = np.concatenate((labelsTrain7,labelsTrain9))
+
+    TEST_DATA = np.array(test7 + test9)
+    TEST_LABELS = np.concatenate((labelsTest7,labelsTest9))
+
+    #TRAIN_LABELS = list(np.array(
+        #np.concatenate((all_7_labels[:(4 * len(all_7_data)) // 5], all_9_labels[:(4 * len(all_9_data)) // 5]))))
+
+    #TEST_DATA = np.array(test7 + test9)
+    #TEST_LABELS = np.array(
+    #    np.concatenate((all_7_labels[(4 * len(all_7_data)) // 5:], all_9_labels[(4 * len(all_9_data)) // 5:])))
+    print("finished loading train and test data")
+    return TRAIN_DATA, TRAIN_LABELS, TEST_DATA, TEST_LABELS
+
 
 class Perceptron(object):
+    def __init__(self, learning_rate, input_shape,epochs):
+        self.lr = learning_rate
+        self.weights = np.random.uniform(-.1,.1,size=input_shape)
+        self.bias = -1
+        self.epochs = epochs
+        self.optimal_success_ratio = 0
+        self.optimal_weights = 0
 
-    def __init__(self, no_of_inputs, epoch=100, learning_rate=0.01):
-        self.epoch = epoch
-        self.learning_rate = learning_rate
-        self.weights = np.zeros(no_of_inputs + 1)
-
-    def predict(self, inputs):
-        summation = np.dot(inputs, self.weights[1:])
-        summation += self.weights[0]
-        try:
-            if summation > 0.0:
-              activation = 1
-            else:
-              activation = 0
-            return activation
-        except ValueError:
-            retVal = []
-            for elem in summation:
-                if elem>0:
-                    retVal.append(1)
+    def single_epoch(self, data, labels):
+        itervals = [i for i in range(len(data))]
+        shuffle(itervals)
+        for i in itervals:
+            prediction = (np.dot(data[i], self.weights)-self.bias) >= 0
+            if prediction != labels[i]:
+                if prediction:
+                    self.weights -= (self.lr*np.array(data[i]))
+                    self.bias -= (self.lr*self.bias)
                 else:
-                    retVal.append(0)
-            return retVal
+                    self.weights += (self.lr*np.array(data[i]))
+                    self.bias += (self.lr*self.bias)
+
+    def train(self, data, labels, testData, testLabels):
+        for i in range(self.epochs):
+            self.single_epoch(data,labels)
+            predictions = np.array(perceptron.predict(testData))
+            correct = predictions == testLabels
+            accuracy = np.sum(correct)/len(testLabels)
+            if accuracy > self.optimal_success_ratio:
+                self.optimal_success_ratio = accuracy
+                self.optimal_weights = self.weights
+
+    def set_weights(self, weights, bias):
+        self.weights = np.array(weights)
+        self.bias = bias
+
+    def predict(self, data):
+        predictions = []
+        for i in range(len(data)):
+            prediction = (np.dot(data[i], self.weights)-self.bias) >= 0
+            predictions.append(prediction)
+        #predictions = [((np.dot(data[i], self.weights)-1) >= 0) for i in range(len(data))]
+
+        return predictions
 
 
-    def train(self, training_inputs, labels):
-        for _ in tqdm.tqdm(range(self.epoch)):
-            for inputs, label in zip(training_inputs, labels):
-                prediction = self.predict(inputs)
-                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-                self.weights[0] += self.learning_rate * (label - prediction)
-
-
-print("Loading train data...")
-# Load the data and generate labels for 1 and 5
-# Since it is basic perceptron, The labels are binary, 1 or 0
-# in this case, 1s are 1, 5s are 0
-all_1_data = generate_labels.LoadAllLabelsOfNumber(1)
-all_1_labels = np.ones((len(all_1_data)))
-all_5_data = generate_labels.LoadAllLabelsOfNumber(5)
-all_5_labels = np.zeros((len(all_5_data)))
-
-print('Creating perceptron...')
-# Create a perceptron with 7 input neurons, 1000 epochs, and a lr of 0.1
-perceptron = Perceptron(len(all_1_data[1]),epoch=1000, learning_rate=0.1)
-
-# Concatenate both training sets into one set
-TRAIN_DATA = np.concatenate((all_1_data,all_5_data))
-TRAIN_LABELS = np.concatenate((all_1_labels,all_5_labels))
-
-print("Training...")
-# Train the perceptron
-perceptron.train(TRAIN_DATA, TRAIN_LABELS)
-print("Training complete...")
-
-# The perceptron has been trained, test it on 7 and 9.
-# note we dont know which label will be assigned to 7 or 9 (although
-# we can probably assume that 7 is 1 and 9 is 5) therefore we are
-# checking to see if the labels are different from one another.
-
-# load the data
-print("Loading test data...")
-all_7_data = generate_labels.LoadAllLabelsOfNumber(7)
-all_9_data = generate_labels.LoadAllLabelsOfNumber(9)
-
-# the project specifications say that only 20% of each dataset should be
-# included in the training process.
-all_7_data = all_7_data[:(len(all_7_data)//5)]
-all_9_data = all_9_data[:(len(all_9_data)//5)]
-
-print("Testing...")
-# generate the labels
-all_7_predicted = perceptron.predict(np.array(all_7_data))
-all_9_predicted = perceptron.predict(np.array(all_9_data))
-print("Testing complete")
-
-# determine the associated label with the number
-label7 = int((np.sum(all_7_predicted)/len(all_7_predicted))>0.5)
-label9 = label7-1
-
-if label7:
-    label7 = np.ones(len(all_7_predicted))
-    label9 = np.zeros(len(all_9_predicted))
-else:
-    label7 = np.zeros(len(all_7_predicted))
-    label9 = np.ones(len(all_9_predicted))
-
-
-# determine the success ratio
-# (success of 7 + success of 9)/total test
-
-success7 = np.sum(label7 == all_7_predicted)
-success9 = np.sum(label9 == all_9_predicted)
-total = len(all_9_predicted)+len(all_7_predicted)
-ratio = (success7+success9)/total
-print("The success ration is: ", ratio)
+TRAIN_DATA, TRAIN_LABELS, TEST_DATA, TEST_LABELS = loadData()
+perceptron = Perceptron(0.1,6,1000)
+perceptron.train(TRAIN_DATA,TRAIN_LABELS, TEST_DATA, TEST_LABELS)
+# the following weights are known to work
+# perceptron.set_weights([-6.37737244, 4.04913265, 46.1064285, -96.24999999, -666.65857, 15.02999999], -1576.899999999)
+predictions = np.array(perceptron.predict(TEST_DATA))
+correct = predictions==TEST_LABELS
+accuracy = np.sum(correct)/len(TEST_LABELS)
+print(accuracy)
+print(perceptron.optimal_success_ratio)
+print(perceptron.optimal_weights)
